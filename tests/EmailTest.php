be3e3906b545a4cc7154736af43a21787a9375a6
Igorgoms <gomesworsnoop@gmail.com>
2024-07-15 23:31:31 -0300

    phpunit-teste

---
+++ tests/EmailTest.php
@@ -0,0 +1,63 @@
+<?php
+class Email
+{
+    private $email;
+    
+    private function __construct(string $email)
+    {
+        $this->validarEmail($email);
+
+        $this->email = $email;
+    }
+
+    public static function retornarInstancia(string $email): self
+    {
+        return new self($email);
+    }
+
+    public function __toString(): string
+    {
+        return $this->email;
+    }
+
+    private function validarEmail(string $email): void
+    {
+        if(!filter_var($email, FILTER_VALIDATE_EMAIL)){
+            throw new \InvalidArgumentException(
+                sprintf(
+                    '"%s" não é um endereço de e-mail válido',
+                    $email
+                )
+            );
+            
+        }
+    }
+
+}
+
+class EmailTest extends PHPUnit\Framework\TestCase
+{
+    public function testValidarTipoDaInstancia(): void
+    {
+        $this->assertInstanceOf(
+            Email::class,
+            Email::retornarInstancia('igorgomes@fridaystudio.com')
+        );
+    }
+
+    public function testTestarRetornoDaInstancia(): void
+    {
+        $this->assertEquals(
+            'jacobkaku@fridaystudio.com',
+            Email::retornarInstancia('jacobkaku@fridaystudio.com')
+        );
+    }
+
+    public function testRetornaExcecaoSeEmailForInvalido(): void
+    {
+        $this->expectException(InvalidArgumentException::class);
+
+        Email::retornarInstancia('jacobkaku@fridaystudio.com');
+    }
+}
+?>
